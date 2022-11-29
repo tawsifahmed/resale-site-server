373f27c86ef21b9dab90e5014db931aa97e6dc1d
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2022-11-29 18:36:21 +0600

    loaded products from mongodb and set api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 4000;
 
 app = express();
@@ -10,19 +11,41 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-const uri = "mongodb+srv://<username>:<password>@tawsifahmed.18d0gsh.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@tawsifahmed.18d0gsh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        const categoriesCollection = client.db('a12-server').collection('categories');
+        const productsCollection = client.db('a12-server').collection('products');
+
+        // categories api
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { id: id };
+            const category = await productsCollection.find(query).toArray();
+            res.send(category);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(er => console.log(er));
 
 app.get('/', async (req, res) => {
     res.send('assignment12 server is running');
 })
 
-app.listen(port, () => console.log(`assignment12 portal running on ${port}`))
+app.listen(port, () => {
+    console.log(`a12 sever running on port ${port}`);
+})
